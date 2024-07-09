27a616dc3e772a3fe54a3aa449966e86d3a935b4
yeiserJimenez <yeiser192@gmail.com>
2024-07-09 15:11:55 -0400

    ♻️ refactor: Update image sources and add local storage support for favorite button

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 const btnFavorite = document.querySelector('#btnFavorite');
-const heartFill = document.querySelector(".hidden")
-const heartEmpty = document.querySelector(".show")
+const heartFill = document.querySelector(".hidden");
+const heartEmpty = document.querySelector(".show");
 
 function toggleFavorite() {
   if (heartFill.classList.contains("hidden")) {
     heartFill.classList.remove("hidden");
     heartEmpty.classList.add("hidden");
+    localStorage.setItem('favorite', true);
   } else {
     heartFill.classList.add("hidden");
     heartEmpty.classList.remove("hidden");
+    localStorage.setItem('favorite', false);
   }
 }
 
 btnFavorite.addEventListener("click", toggleFavorite);
+
+const isFavorite = localStorage.getItem('favorite');
+if (isFavorite === 'true') {
+  heartFill.classList.remove("hidden");
+  heartEmpty.classList.add("hidden");
+} else {
+  heartFill.classList.add("hidden");
+  heartEmpty.classList.remove("hidden");
+}
